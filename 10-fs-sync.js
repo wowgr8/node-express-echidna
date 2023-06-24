0b9cdc1b6c184dd6a5a5d1bc89ebfd3fd857da29
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-06-23 22:03:57 -0700

    add flag as third parameter in fs.writeFileSync() - aka append method.

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -8,4 +8,8 @@ const second = readFileSync('./content/second.txt', 'utf8')
 console.log(first, second)
 
 // If file does not exist, node will create it. First parameter is the path, second is the data.
-writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`)
+writeFileSync(
+  './content/result-sync.txt',
+  `Here is the result: ${first}, ${second}`,
+  { flag: 'a'} // if you want to append something to the file, you will have to pass a third parameter. 
+  )
