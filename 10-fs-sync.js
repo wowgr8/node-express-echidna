a6e64dff00ae7bdeec5069514410a9333d400b94
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-06-23 21:52:02 -0700

    add two variables reading data with readFileSync() in .txt files

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,3 +1,8 @@
-const { readFileSync, writeFileSync } = require(fs)
+const { readFileSync, writeFileSync } = require('fs')
 // const fs = require(fs)   These two lines do the same as line 1
 // fs.read
+
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
+
+console.log(first, second)
