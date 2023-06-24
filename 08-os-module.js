3363db672657931e3c265d048f30f2e1f201ac16
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-06-23 19:31:34 -0700

    add os built-in module w/ method examples.

---
+++ 08-os-module.js
@@ -0,0 +1,16 @@
+const os = require('os') //Built in module
+
+// info about current user
+const user = os.userInfo()
+console.log(user) 
+
+// Method returns the system uptime in seconds
+console.log(`The System Uptime is : ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+}
+console.log(currentOS);
