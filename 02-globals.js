e61be700848c8b725ad8c0b793cee21c3f0de555
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-06-23 18:00:41 -0700

    add global variable console.log() and setInterval()

--- 02-globals.js
+++ 02-globals.js
@@ -6,3 +6,7 @@
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
 
+console.log(__dirname);
+setInterval(() => {
+  console.log('Hello world')
+}, 1000)
