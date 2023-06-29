2fb6799341b61d9cdddb87f210116ee889b096c7
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-06-28 19:59:30 -0700

    add url paths examples

--- 12-http.js
+++ 12-http.js
@@ -1,8 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-  res.write('Welcome to our home page');
-  res.end()
+  //console.log(req) //Will log after page is refreshed.
+  if(req.url === '/'){
+    res.end('Welcome to our home page')
+  }
+  if(req.url === '/about'){
+    res.end('Here is our short history')
+  }
+  res.end(`
+    <h1>Oops!</h1>
+    <p>We can't find the page you are looking for</p>
+    <a href="/">back home</a>
+  `)
 })
 
 server.listen(5000);
