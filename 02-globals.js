0ebab47cf7cd5e30a33198a0fd599d86f1955cb6
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-10-09 15:44:57 -0700

    WIP

--- 02-globals.js
+++ 02-globals.js
@@ -6,7 +6,7 @@
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
 
+// Answers for Code The Dream lesson:
 console.log(__dirname);
-setInterval(() => {
-  console.log('Hello world')
-}, 1000)
+
+// const export MY_VAR="Hi there!" 
